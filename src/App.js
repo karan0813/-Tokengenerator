2979ec8500480178e84a78368f099833cc9da96f
karan0813 <karanbandekar0813@gmail.com>
2024-09-03 10:14:58 -0700

    implemented state management for input field component

--- src/App.js
+++ src/App.js
@@ -1,18 +1,78 @@
-import React from "react";
+import React, { useState } from "react";
 import InputBox from "./Component/InputBox";
 
 function App() {
+  // state
+  const [state, setState] = useState({
+    inputBlueCount: 0,
+    inputBluePrefix: "",
+    inputBluePerRow: 1,
+    inputRedCount: 0,
+    inputRedPrefix: "",
+    inputRedPerRow: 1,
+    blueTokens: [],
+    redTokens: [],
+    bluePerRow: 1,
+    redPerRow: 1,
+  });
+
+  const handleChange = (e) => {
+    const { name, value } = e.target;
+    setState((prevState) => ({
+      ...prevState,
+      [name]: value,
+      // [name]: name.includes("PerRow") ? Number(value) : value,
+    }));
+    console.log(state);
+  };
+
   const handleGenerate = () => {};
   const handleClear = () => {};
   return (
     <div className="max-w-md mx-auto p-6 bg-white rounded-md shadow-md">
       <div className="space-y-4">
-        <InputBox label={"Number of Blue Tokens"} InputType="number" />
-        <InputBox label={"Blue Token Prefix"} InputType="text" />
-        <InputBox label={"Blue Tokens per Row"} InputType="number" />
-        <InputBox label={"Number of Red Tokens"} InputType="number" />
-        <InputBox label={"Red Token Prefix"} InputType="text" />
-        <InputBox label={"Red Tokens per Row"} InputType="number" />
+        <InputBox
+          label={"Number of Blue Tokens"}
+          InputType="number"
+          inputName="inputBlueCount"
+          value={state.inputBlueCount}
+          handleChange={handleChange}
+        />
+        <InputBox
+          label={"Blue Token Prefix"}
+          InputType="text"
+          inputName="inputBluePrefix"
+          value={state.inputBluePrefix}
+          handleChange={handleChange}
+        />
+        <InputBox
+          label={"Blue Tokens per Row"}
+          InputType="number"
+          inputName="inputBluePerRow"
+          value={state.inputBluePerRow}
+          handleChange={handleChange}
+        />
+        <InputBox
+          label={"Number of Red Tokens"}
+          InputType="number"
+          inputName="inputRedCount"
+          value={state.inputRedCount}
+          handleChange={handleChange}
+        />
+        <InputBox
+          label={"Red Token Prefix"}
+          InputType="text"
+          inputName="inputRedPrefix"
+          value={state.inputRedPrefix}
+          handleChange={handleChange}
+        />
+        <InputBox
+          label={"Red Tokens per Row"}
+          InputType="number"
+          inputName="inputRedPerRow"
+          value={state.inputRedPerRow}
+          handleChange={handleChange}
+        />
         {/* Generate and Clear Buttons */}
         <div className="space-x-4">
           <button
--- src/Component/InputBox.jsx
+++ src/Component/InputBox.jsx
@@ -1,11 +1,14 @@
 import React from "react";
 
-const InputBox = ({ label, value, setValue, InputType }) => {
+const InputBox = ({ label, inputName, value, InputType, handleChange }) => {
   return (
     <div>
       <label className="block text-gray-700">{label}</label>
       <input
+        name={inputName}
         type={InputType}
+        value={value}
+        onChange={handleChange}
         className="w-full p-2 border border-gray-300 rounded"
       />
     </div>
