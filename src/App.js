cc6f160a90ab7709bfd99c53a81c5747c2bbc7d2
karan0813 <karanbandekar0813@gmail.com>
2024-09-03 21:48:56 -0700

    changed button text

--- src/App.js
+++ src/App.js
@@ -82,6 +82,7 @@ function App() {
   return (
     <div className="max-w-md mx-auto p-6 bg-white rounded-md shadow-md form ">
       <div className="space-y-4">
+        <h3>Token Generator</h3>
         <InputBox
           label={"Number of Blue Tokens"}
           InputType="number"
@@ -130,7 +131,7 @@ function App() {
             onClick={handleGenerate}
             className="px-4 py-2 bg-blue-500 text-white rounded hover:bg-blue-600 w-full"
           >
-            Generate
+            Generate Token
           </button>
           <button
             onClick={handleClear}
